ff15a9b9572b5635a76a01b080fed6c54b223410
mda84 <97781077+mda84@users.noreply.github.com>
2022-04-25 21:25:07 -0700

    Update pose_decode.py

--- LightweightOpenPose/common/pose_decode.py
+++ LightweightOpenPose/common/pose_decode.py
@@ -4,8 +4,8 @@ import math
 import os, sys
 sys.path.append('../..')
 
-#heatmap_width = 92
-#heatmap_height = 92
+heatmap_width = 92
+heatmap_height = 92
 
 """
 Joints Explained
@@ -32,21 +32,21 @@ Joints Explained
     # L_Elbow = 6, L_Wrist = 7, R_Hip = 8, R_Knee = 9, R_Ankle = 10, L_Hip = 11
     # L_Knee = 12, L_Ankle = 13, R_Eye = 14, L_Eye = 15, R_Ear = 16, L_Ear = 17
 
-JOINT_LIMB = [[16, 14], [14, 0], [0, 15], [15, 17], [0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 8], [1, 11], [8, 9], [9, 10], [11, 12], [12, 13]]
-#JOINT_LIMB = [[0, 1], [1, 2], [3, 4], [4, 5], [6, 7], [7, 8], [9, 10], [10, 11], [12, 13], [13, 0], [13, 3], [13, 6], [13, 9]]
+JOINT_LIMB17 = [[16, 14], [14, 0], [0, 15], [15, 17], [0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 8], [1, 11], [8, 9], [9, 10], [11, 12], [12, 13]]
+JOINT_LIMB14 = [[0, 1], [1, 2], [3, 4], [4, 5], [6, 7], [7, 8], [9, 10], [10, 11], [12, 13], [13, 0], [13, 3], [13, 6], [13, 9]]
 COLOR = [[0, 255, 255], [0, 255, 255],[0, 255, 255],[0, 255, 255],[0, 255, 0],[0, 255, 0],[0, 255, 0],[0, 255, 0], [0, 0, 255], [255, 0, 0],[255, 0, 0],[255, 0, 0], [255, 0, 0], [0, 255, 255], [0, 255, 255],[0, 255, 255], [0, 255, 0]]
 
-def decode_pose(heatmaps, scale, image_original, state, confidence):
+def decode_pose(heatmaps, scale, image_original):#, state, confidence):
 
     # obtain joint list from heatmap
     # joint_list: a python list of joints, joint_list[i] is an numpy array with the (x,y) coordinates of the i'th joint (refer to the 'Joints Explained' in this file, e.g., 0th joint is right shoulder)  
     
-    #joint_list = [peak_index_to_coords(heatmap)*scale for heatmap in heatmaps]
-    #print(scale)
+    joint_list = [peak_index_to_coords(heatmap)*scale for heatmap in heatmaps]
+    #print(len(joint_list),joint_list)
     #print(heatmaps)
-    joint_list = [heatmap*scale for heatmap in heatmaps]
+    #joint_list = [heatmap*scale for heatmap in heatmaps]
 
-    for i, joint in enumerate(joint_list):
+    '''for i, joint in enumerate(joint_list):
         if state[i, 0, 0]!=0 or state[i, 1, 0]!=0 or state[i, 0, 1]!=0 or state[i, 1, 1]!=0:
             joint_list[i][0] = confidence[i] * joint_list[i][0] + (1-confidence[i]) * (state[i, 0, 0] + state[i, 1, 0])
             joint_list[i][1] = confidence[i] * joint_list[i][1] + (1-confidence[i]) * (state[i, 0, 1] + state[i, 1, 1])
@@ -55,19 +55,19 @@ def decode_pose(heatmaps, scale, image_original, state, confidence):
             if joint[0]!=0 or joint[1]!=0:
                 joint_list[i] = joint
             else:
-                return None, []
+                return None, []'''
                 
     #print(joint_list)
     
     # plot the pose on original image
     canvas = image_original
-    for idx, limb in enumerate(JOINT_LIMB):
+    for idx, limb in enumerate(JOINT_LIMB14):
         joint_from, joint_to = joint_list[limb[0]], joint_list[limb[1]]
         canvas = cv2.line(canvas, tuple(joint_from.astype(int)), tuple(joint_to.astype(int)), color=COLOR[idx], thickness=4)
     
     return canvas, joint_list   
 
-'''
+
 def peak_index_to_coords(peak_index):
     
     #@peak_index is the index of max value in flatten heatmap
@@ -75,7 +75,7 @@ def peak_index_to_coords(peak_index):
     
     peak_coords = np.unravel_index(int(peak_index),(heatmap_height, heatmap_width))
     return np.flip(peak_coords)
-'''
+
 
 def body_pose_to_h36(body_pose_keypoints):
     # Body Pose Order
@@ -97,59 +97,58 @@ def body_pose_to_h36(body_pose_keypoints):
     for i in range (0, body_pose_keypoints.shape[0]):
         keypoints = np.zeros([17, 2])
         
-        # Pelvis
-        keypoints[0][0] = (body_pose_keypoints[i][8][0] + body_pose_keypoints[i][11][0]) / 2
-        keypoints[0][1] = (body_pose_keypoints[i][8][1] + body_pose_keypoints[i][11][1]) / 2
+        # Pelvis ***
+        keypoints[0][0] = (body_pose_keypoints[i][6][0] + body_pose_keypoints[i][9][0]) / 2
+        keypoints[0][1] = (body_pose_keypoints[i][6][1] + body_pose_keypoints[i][9][1]) / 2
 
         # Right Hip
-        keypoints[1] = body_pose_keypoints[i][8]
+        keypoints[1] = body_pose_keypoints[i][6]
 
         # Right Knee
-        keypoints[2] = body_pose_keypoints[i][9]
+        keypoints[2] = body_pose_keypoints[i][7]
 
         # Right Foot
-        keypoints[3] = body_pose_keypoints[i][10]
+        keypoints[3] = body_pose_keypoints[i][8]
 
         # Left Hip
-        keypoints[4] = body_pose_keypoints[i][11]
+        keypoints[4] = body_pose_keypoints[i][9]
 
         # Left Knee
-        keypoints[5] = body_pose_keypoints[i][12]
+        keypoints[5] = body_pose_keypoints[i][10]
 
         # Left Foot
-        keypoints[6] = body_pose_keypoints[i][13]
+        keypoints[6] = body_pose_keypoints[i][11]
 
-        # Spine
-        keypoints[7][0] = (body_pose_keypoints[i][2][0] + body_pose_keypoints[i][5][0] + body_pose_keypoints[i][8][0] + body_pose_keypoints[i][11][0]) / 4
-        keypoints[7][1] = (body_pose_keypoints[i][2][1] + body_pose_keypoints[i][5][1] + body_pose_keypoints[i][8][1] + body_pose_keypoints[i][11][1]) / 4
+        # Spine ***
+        keypoints[7][0] = (body_pose_keypoints[i][0][0] + body_pose_keypoints[i][3][0] + body_pose_keypoints[i][6][0] + body_pose_keypoints[i][9][0]) / 4
+        keypoints[7][1] = (body_pose_keypoints[i][0][1] + body_pose_keypoints[i][3][1] + body_pose_keypoints[i][6][1] + body_pose_keypoints[i][9][1]) / 4
 
         # Thorax
-        keypoints[8] = body_pose_keypoints[i][1]
+        keypoints[8] = body_pose_keypoints[i][13]
 
         # Nose
-        keypoints[9] = body_pose_keypoints[i][0]
+        keypoints[9] = body_pose_keypoints[i][12]
 
         # Head
-        keypoints[10][0] = (body_pose_keypoints[i][14][0] + body_pose_keypoints[i][15][0]) / 2
-        keypoints[10][1] = (body_pose_keypoints[i][14][1] + body_pose_keypoints[i][15][1]) / 2
+        keypoints[10] = body_pose_keypoints[i][12]
 
         # Left Shoulder
-        keypoints[11] = body_pose_keypoints[i][5]
+        keypoints[11] = body_pose_keypoints[i][3]
 
         # Left Elbow
-        keypoints[12] = body_pose_keypoints[i][6]
+        keypoints[12] = body_pose_keypoints[i][4]
 
         # Left Wrist
-        keypoints[13] = body_pose_keypoints[i][7]
+        keypoints[13] = body_pose_keypoints[i][5]
 
         # Right Shoulder
-        keypoints[14] = body_pose_keypoints[i][2]
+        keypoints[14] = body_pose_keypoints[i][0]
 
          # Right Elbow
-        keypoints[15] = body_pose_keypoints[i][3]
+        keypoints[15] = body_pose_keypoints[i][1]
 
          # Right Wrist
-        keypoints[16] = body_pose_keypoints[i][4]
+        keypoints[16] = body_pose_keypoints[i][2]
 
         h36_keypoints.append(keypoints)
 
